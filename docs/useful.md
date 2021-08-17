a00a2e9112b4af4c1ea61b570003591acbf3c9af
Raissa C Correia <raisssa.correia@gmail.com>
2021-08-17 12:16:42 -0300

    started comparison material vs chakra vs mine

---
+++ docs/useful.md
@@ -0,0 +1,2 @@
+https://www.figma.com/file/CNXMpZnv5c5o5YqUTm3JD3/Raissa-UI-Kit?node-id=0%3A1
+https://www.youtube.com/watch?v=RYDiDpW2VkM&t=268s
---
+++ raissa-uikit/components/atoms/buttons/BasicButton.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function BasicButton() {
+    return (
+        <button type="button">Click Me!</button>
+    )
+}
